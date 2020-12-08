95e86392643e4ac94ab720fb2ea8281eaee4208d
Bchaises <benjamin.chaises@gmail.com>
2020-12-08 10:47:56 +0100

    Create MainWindow

--- src/Bchaises/Main.java
+++ src/Bchaises/Main.java
@@ -1,8 +1,19 @@
 package Bchaises;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Chanson c = new Chanson();
+        Playlist p = new Playlist();
+
+        p.add(c);
+
+        JFrame laFenetre = new MainWindow();
+        laFenetre.setVisible(true);
+
+        laFenetre.getPlaylist().add(c);
     }
 }
--- src/Bchaises/MainWindow.java
+++ src/Bchaises/MainWindow.java
@@ -1,4 +1,32 @@
 package Bchaises;
 
-public class MainWindow {
+import javax.swing.*;
+
+public class MainWindow extends JFrame{
+
+    private JButton playButton;
+    private JPanel MainContent;
+    private JButton prevButton;
+    private JButton pauseButton;
+    private JButton nextButton;
+    private JLabel pauseLabel;
+    private JLabel playLabel;
+    private JLabel previousLabel;
+    private JLabel nextLabel;
+    private JSlider slider1;
+    private Playlist playlist;
+
+    MainWindow(){
+        super();
+        this.setContentPane(MainContent);
+        this.pack();
+    }
+
+    //setter
+    public void setPlaylist(Playlist p) { this.playlist = p; }
+
+    //getter
+    public Playlist getPlaylist() { return this.playlist; }
+
+
 }
